b92960752d3ee031a36b3788be0b9b42a2b3baf1
Anurag <anurag.baranwal@Kuliza.com>
2023-06-20 06:48:13 +0530

    excel sheet to db started

---
+++ src/main/java/com/excel/ExcelsheetApplication.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelsheetApplication.class, args);
+	}
+
+}
